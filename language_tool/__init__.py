bc0892501c26749fa65fcd46141aa246d46dd6b2
spirit <none@none>
2012-06-14 18:50:50 +0200

    License as comment block

--- language_tool/__init__.py
+++ language_tool/__init__.py
@@ -1,21 +1,20 @@
-"""LanguageTool through server mode"""
-'''
-    © 2012 spirit <hiddenspirit@gmail.com>
-    https://bitbucket.org/spirit/language_tool/
-
-    This program is free software: you can redistribute it and/or modify it
-    under the terms of the GNU Lesser General Public License as published
-    by the Free Software Foundation, either version 3 of the License,
-    or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty
-    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
-'''
+"""LanguageTool through server mode
+"""
+#   © 2012 spirit <hiddenspirit@gmail.com>
+#   https://bitbucket.org/spirit/language_tool/
+#
+#   This program is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published
+#   by the Free Software Foundation, either version 3 of the License,
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import atexit
 import glob
