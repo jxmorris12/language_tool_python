4bc101abbc0f43ebee6a30fff358b4f910fb2439
spirit <none@none>
2012-07-04 01:54:32 +0200

    Download for relevant commands only.

--- setup_language_tool.py
+++ setup_language_tool.py
@@ -6,5 +6,7 @@ from download_lt import download_lt
 def hook(config):
     if "sdist" in sys.argv:
         del config["files"]["package_data"]
-    else:
+    elif any(arg.startswith("install") or
+             arg.startswith("build") or
+             arg.startswith("bdist") for arg in sys.argv):
         download_lt()
