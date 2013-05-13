a7f5872b25a3863d26b1336300c2272eb7d58b36
Steven Myint <git@stevenmyint.com>
2013-05-12 18:18:38 -0700

    Test preferred command

--- test.bash
+++ test.bash
@@ -2,5 +2,7 @@
 #
 # Test command-line usage.
 
+echo 'This is okay.' | language-tool -
+! echo 'This is noot okay.' | language-tool -
+
 echo 'This is okay.' | python -m language_tool -
-! echo 'This is noot okay.' | python -m language_tool -
