d911acd16d90bb3b0fed2b7ec68b534a10887f50
Steven Myint <git@stevenmyint.com>
2014-09-06 08:08:10 -0700

    Print message on failure

--- test.bash
+++ test.bash
@@ -2,6 +2,8 @@
 #
 # Test command-line usage.
 
+trap "echo -e '\x1b[01;31mFailed\x1b[0m'" ERR
+
 echo 'This is okay.' | language-check -
 ! echo 'This is noot okay.' | language-check -
 
@@ -12,3 +14,5 @@ echo 'These are “smart” quotes.' | python -m language_check -
 ! echo 'These are "dumb" quotes.' | python -m language_check -
 
 echo '# These are "dumb".' | python -m language_check --ignore-lines='^#' -
+
+echo -e '\x1b[01;32mOkay\x1b[0m'
