81bd27465bc7009a1a1307fdd40eee97dd1efff6
Jack Morris <jxmorris12@gmail.com>
2020-05-01 12:39:58 -0400

    update tests

--- test.bash
+++ test.bash
@@ -6,8 +6,8 @@ set -ex
 
 trap "echo -e '\x1b[01;31mFailed\x1b[0m'" ERR
 
-echo 'This is okay.' | language_tool_python -
-! echo 'This is noot okay.' | language_tool_python -
+echo 'This is okay.' | python -m language_tool_python -
+! echo 'This is noot okay.' | python -m language_tool_python -
 
 echo 'This is okay.' | python -m language_tool_python -
 ! echo 'This is noot okay.' | python -m language_tool_python -
--- test_remote.bash
+++ test_remote.bash
@@ -5,7 +5,7 @@ set -eux
 readonly port=8081
 
 # TODO: Find this more portably.
-readonly jar='./build/lib/language_tool_python/LanguageTool-3.1/languagetool-server.jar'
+readonly jar='./language_tool_python/LanguageTool-*/languagetool-server.jar'
 
 java -cp "$jar" org.languagetool.server.HTTPServer --port "$port" &
 java_pid=$!
@@ -17,7 +17,7 @@ clean ()
 trap clean EXIT
 
 echo 'This is okay.' | \
-    language_tool_python --remote-host localhost --remote-port "$port" -
+    python -m language_tool_python --remote-host localhost --remote-port "$port" -
 
 ! echo 'This is noot okay.' | \
-    language_tool_python --remote-host localhost --remote-port "$port" -
+    python -m language_tool_python --remote-host localhost --remote-port "$port" -
