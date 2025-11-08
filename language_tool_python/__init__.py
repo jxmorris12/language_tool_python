35054fa325f879e86598430fe8df362dfd5e2ec7
mdevolde <martin.devolder2@gmail.com>
2025-11-09 00:45:34 +0100

    refactor (__ini__.py) : declared imports in __all__ (ruff patch)

--- language_tool_python/__init__.py
+++ language_tool_python/__init__.py
@@ -1,5 +1,13 @@
 """LanguageTool API for Python."""
 
+__all__ = [
+    'LanguageTool',
+    'LanguageToolPublicAPI',
+    'LanguageTag',
+    'Match',
+    'utils',
+]
+
 from .language_tag import LanguageTag
 from .match import Match
 from .server import LanguageTool, LanguageToolPublicAPI
