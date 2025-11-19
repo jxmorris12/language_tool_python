b37e7864e3faafc085936f86f903a62200d4d12e
mdevolde <martin.devolder2@gmail.com>
2025-11-19 17:07:16 +0100

    feat (build_and_publish): moved build and publish script in Makefiles, used uv instead of twine

--- build_and_publish.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build/ dist/ *.egg-info/
-python -m pip install --upgrade pip setuptools wheel build
-python -m build
-twine check dist/*
-twine upload dist/* --verbose
--- make.bat
+++ make.bat
@@ -3,8 +3,9 @@
 if "%1"=="check" goto check
 if "%1"=="test" goto test
 if "%1"=="doc" goto doc
+if "%1"=="publish" goto publish
 
-echo Usage: make.bat [check^|test^|doc]
+echo Usage: make.bat [check^|test^|doc^|publish]
 exit /b 1
 
 :check
@@ -19,10 +20,29 @@ exit /b %errorlevel%
 
 :test
 pytest
+if errorlevel 1 exit /b %errorlevel%
+
 uvx --with defusedxml genbadge coverage --input-file coverage.xml --silent
 exit /b %errorlevel%
 
 :doc
 uv sync --group tests --group docs --group types
+
 call .venv\Scripts\activate && uv run sphinx-apidoc -o docs\source\references language_tool_python
+if errorlevel 1 exit /b %errorlevel%
+
 call .venv\Scripts\activate && call docs\make.bat html
+exit /b %errorlevel%
+
+:publish
+if exist dist\ rmdir /s /q dist\
+if exist language_tool_python.egg-info\ rmdir /s /q language_tool_python.egg-info\
+
+uv build
+if errorlevel 1 exit /b %errorlevel%
+
+uvx twine check .\dist\*
+if errorlevel 1 exit /b %errorlevel%
+
+uv publish
+exit /b %errorlevel%
